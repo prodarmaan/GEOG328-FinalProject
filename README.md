ad98e4d32b05aabc1b3564047a46fb101158ea9d
ARMAAN <106575511+prodarmaan@users.noreply.github.com>
2023-12-14 17:50:26 -0800

    Updated
    - added files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# GEOG328-FinalProject
+## GEOG 328: Group 10
+# Mapping Seattle Income and 911 Emergency Calls
+*Armaan Azeem, Phoebe Mace, Amelia Huang, Gengchen Dai*
+
+### Description
+This application maps the per-capita income (PCI) of Seattle's census tracts and the amount of 911 emergency calls. The PCI layer is represented as an interactive chloropleth layer, and the 911 calls are represented as a dot-density later. The application also includes a table of census tract information, along with sorting functionality.
+
+### Project Goal
+The goal of this project is to implement skills and techniques developed throughout this course to create an interactive map application. The overall purpose of the map is to illustrate the geospatial relationship between Seattle's census tract CPI and location of 911 emergency calls. At a glance, the overall relationship appears to be that areas of higher PCI seem to have a more concentrated amount of 911 emergency calls. The target audience of this map is Seattle residents, government officials, researchers and the general public interested in understanding the relationship between the income and the number of 911 calls in the city.
+
+Application URL: https://phoebermace.github.io/GEOG328-group10_final/
+
+### Map Preview
+![map_preview](img/updatedpreview.png)
+
+### Main Functions
+- Sorting Table by Per Capita Income
+- Sorting Table by Population
+- Interactive Hover over Chloropleth Layer
+
+### Data Sources
+- [Per Capita Income and Aggregate Income in the Past 12 Months](https://data-seattlecitygis.opendata.arcgis.com/datasets/SeattleCityGIS::per-capita-income-and-aggregate-income-in-the-past-12-months-in-inflation-adjusted-dollars/explore) 
+- [Seattle Fire 911 Calls](https://data.seattle.gov/Public-Safety/Seattle-Real-Time-Fire-911-Calls/kzjm-xkqj) 
+- [2020 Census Tracts - Seattle](https://data.seattle.gov/dataset/2020-Census-Tracts-Seattle/yf4x-8yiw/data) 
+
+### Active Applied Libraries & Web Services
+- Mapbox GL JS
+- Mapbox Basemap
+- Github 
+
+### Acknowledgement
+This map application is developed for GEOG 328: Web-Based GIS at the University of Washington, Seattle.
+
+### Other Notes
+Sometimes, the chloropleth layer does not load on the first launch. If this error occurs, refresh the page.
---
+++ assets/file analysis.R
@@ -0,0 +1,91 @@
+# Loading required libraries
+library(tidyverse)
+library(sf)
+library(dplyr)
+library(leaflet)
+library(classInt)
+
+# Set working directory
+setwd("/Users/armvg/Desktop/CLASSES/2023-2024/FALL")
+
+# Loading in 911 Calls data
+calls <- read.csv("filtered911Calls.csv")
+
+# Loading in Per Capita Income data
+income <- read.csv("PerCapitaIncome.csv")
+
+# Reformatting the 'Datetime' column format for filtering compatibility
+calls$Datetime <- as.POSIXct(strptime(calls$Datetime, format="%m/%d/%Y %I:%M:%S %p"))
+
+# Filtering the 911 calls dataset to only include the relevant time frame
+lowerThreshold <- as.POSIXct("2022-08-31 00:00:00", format="%Y-%m-%d %H:%M:%S", tz="UTC")
+upperThreshold <- as.POSIXct("2023-08-31 00:00:00", format="%Y-%m-%d %H:%M:%S", tz="UTC")
+newCalls <- filter(calls, (calls$Datetime > lowerThreshold & calls$Datetime < upperThreshold))
+
+# Seattle Census Tracts GEOJSON 
+geojson <- st_read("censusTracts.geojson")
+
+# Format Columns
+income$GEOID <- as.character(income$GEOID)
+geojson$GEOID20 <- as.character(geojson$GEOID20)
+
+# Left Join
+merged <- geojson %>%
+  left_join(income, by = c("GEOID20" = "GEOID"))
+
+# Format GEOJSON
+format_properties <- function(row) {
+  return(list(
+    OBJECTID = row$OBJECTID,
+    GEOID20 = row$GEOID20,
+    GROSS_ACRES = row$GROSS_ACRES,
+    LAND_ACRES = row$LAND_ACRES,
+    WATER_ACRES = row$WATER_ACRES,
+    NAME = row$NAME,
+    TRACT_NUMB = row$TRACT_NUMB,
+    BASENAME = row$BASENAME,
+    UVDA_AREA = row$UVDA_AREA,
+    CRA_NO = row$CRA_NO,
+    CRA_GRP = row$CRA_GRP,
+    GEN_ALIAS = row$GEN_ALIAS,
+    DETL_NAMES = row$DETL_NAMES,
+    C_DISTRICT = row$C_DISTRICT,
+    SHAPE_Length = row$SHAPE_Length,
+    SHAPE_Area = row$SHAPE_Area,
+    PCIAdjusted = row$PCIAdjusted,
+    geometry = row$geometry
+  ))
+}
+
+# Apply the function to each row
+merged$properties <- purrr::map(1:nrow(merged), function(i) format_properties(merged[i, ]))
+
+# Remove the original "properties" column
+merged <- merged %>% select(-properties)
+
+# Aggregating Per Capita Income data
+merged <- merged %>%
+  group_by(BASENAME, CRAName) %>%
+  summarise(
+    avg_income = mean(PCIAdjusted),
+    total_population = sum(Population))
+
+# Export the new GEOJSON to a temporary file
+temp_file <- tempfile(fileext = ".geojson")
+st_write(merged, temp_file, driver = "GeoJSON", delete_layer = TRUE)
+
+# Move the temporary file to replace the original
+file.rename(temp_file, "PCI.geojson")
+
+# Finding PCI Natural Breaks
+
+# Minimum
+min <- min(merged$avg_income)
+
+# Maximum 
+max <- max(merged$avg_income)
+
+# Natural Breaks Scheme
+intervals <- classIntervals(merged$avg_income, n = 5, style = "jenks")
+breaks <- intervals$brks
+cat("Class Breaks:", breaks, "\n")
